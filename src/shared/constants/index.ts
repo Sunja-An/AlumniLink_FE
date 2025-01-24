dff14c9fa74f4ad378f7a83ae83505347b11299e
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-01-24 21:44:49 +0900

    Feat: Add Controller Static Data

---
+++ src/shared/constants/index.ts
@@ -0,0 +1,27 @@
+import AlumniFlag from "/public/svg/alumni.svg";
+import Graduate from "/public/svg/graduate.svg";
+import My from "/public/svg/my.svg";
+import Question from "/public/svg/question.svg";
+import RightArrow from "/public/svg/RightArrow.svg";
+import Search from "/public/svg/SearchIcon.svg";
+import SignOut from "/public/svg/signout.svg";
+
+import Github from "/public/sns/GithubMark.svg";
+import Instagram from "/public/sns/insta.svg";
+
+import Logo from "/public/logo/logo.png";
+import LogoNoBackground from "/public/logo/LogoNoBg.png";
+
+export const ICON_ALUMNIFLAG = AlumniFlag;
+export const ICON_GRADUATE = Graduate;
+export const ICON_MY = My;
+export const ICON_QUESTION = Question;
+export const ICON_ARROW = RightArrow;
+export const ICON_SEARCH = Search;
+export const ICON_SIGNOUT = SignOut;
+
+export const ICON_GITHUB = Github;
+export const ICON_INSTAGRAM = Instagram;
+
+export const IMG_LOGO = Logo;
+export const IMG_NO_BG_LOGO = LogoNoBackground;
