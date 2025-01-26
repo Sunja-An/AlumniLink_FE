a287c2065d94dbdf52237750a61a135d0d9d8d37
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-01-26 20:12:10 +0900

    Feat: Add constants

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -5,6 +5,8 @@ import Question from "/public/svg/question.svg";
 import RightArrow from "/public/svg/RightArrow.svg";
 import Search from "/public/svg/SearchIcon.svg";
 import SignOut from "/public/svg/signout.svg";
+import Image from "/public/svg/image.svg";
+import Docs from "/public/svg/docs.svg";
 
 import Github from "/public/sns/GithubMark.svg";
 import Instagram from "/public/sns/insta.svg";
@@ -16,18 +18,26 @@ export const HEADER_CONTENT = [
   {
     id: 1,
     content: "정보 찾기",
+    url: "info?page=0&size=0",
+    isLoginNeed: false,
   },
   {
     id: 2,
     content: "프로젝트 찾기",
+    url: "project?page=0&size=0",
+    isLoginNeed: false,
   },
   {
     id: 3,
     content: "마이 페이지",
+    url: "my",
+    isLoginNeed: true,
   },
   {
     id: 4,
-    content: "설정",
+    content: "로그아웃",
+    url: "",
+    isLoginNeed: true,
   },
 ];
 
@@ -38,6 +48,8 @@ export const ICON_QUESTION = Question;
 export const ICON_ARROW = RightArrow;
 export const ICON_SEARCH = Search;
 export const ICON_SIGNOUT = SignOut;
+export const ICON_IMAGE = Image;
+export const ICON_DOCS = Docs;
 
 export const ICON_GITHUB = Github;
 export const ICON_INSTAGRAM = Instagram;
