b0da01357252a381a65909a9be2b37cff604100d
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-01-28 23:26:39 +0900

    Design: CSS Modified

--- src/shared/layout/CustomLayout.tsx
+++ src/shared/layout/CustomLayout.tsx
@@ -8,14 +8,14 @@ type T_CustomLayout = {
 
 function CustomLayout({ isHeader, children }: T_CustomLayout) {
   return (
-    <main className="min-w-screen min-h-screen flex justify-start items-start overflow-hidden bg-[#F0F0F0]">
+    <main className="min-w-screen min-h-screen flex justify-start items-start overflow-hidden bg-[#E2E2E2]">
       {isHeader && (
-        <section className="fixed top-0 left-0 min-w-80 w-80 min-h-screen h-screen lg:block hidden">
+        <section className="fixed ml-10 my-10 top-0 left-0 min-w-80 w-80 h-[90%] lg:block hidden">
           <Header />
         </section>
       )}
       <section
-        className={`py-20 w-full h-full flex justify-start items-start ${
+        className={`py-10 w-full h-full flex justify-start items-start ${
           isHeader && "lg:ml-80"
         }`}
       >
