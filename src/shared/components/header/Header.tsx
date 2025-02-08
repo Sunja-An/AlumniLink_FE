a669ada823e35579ac47ed720afcea7dfd508525
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-02-08 13:12:12 +0900

    ✨ Update Header design

--- src/shared/components/header/Header.tsx
+++ src/shared/components/header/Header.tsx
@@ -1,12 +1,35 @@
 "use client";
 
 import { ICON_GITHUB, ICON_INSTAGRAM } from "@/shared/constants";
+import { cn } from "@/shared/utils/clsx";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 30);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="px-40 w-full h-16 min-h-16 flex justify-between items-center bg-secondary">
+    <header
+      className={cn(
+        "fixed top-0 left-0 px-40 w-full min-h-14 flex justify-between items-center duration-300 z-10",
+        {
+          "bg-secondary h-14 shadow-lg": isScrolled,
+          "bg-transparent h-16": !isScrolled,
+        }
+      )}
+    >
       <div className="w-2/3 h-full flex justify-center items-center">
         <div className="px-10 w-3/4 h-3/4">
           <input
@@ -14,15 +37,19 @@ function Header() {
             name="search"
             id="search"
             placeholder="검색어를 입력해주세요."
-            className="px-10 py-4 w-full h-full rounded-full focus:outline-none"
+            className="px-10 py-4 w-full h-full font-pretendard font-medium text-sm rounded-full focus:outline-none"
           />
         </div>
       </div>
       <div className="w-1/3 h-full flex justify-end items-center gap-4">
-        <Image src={ICON_GITHUB} alt="github" width={28} height={28} />
-        <Image src={ICON_INSTAGRAM} alt="instagram" width={28} height={28} />
+        <div className="w-10 h-10 rounded-md flex justify-center items-center hover:bg-slate-200 duration-300">
+          <Image src={ICON_GITHUB} alt="github" width={28} height={28} />
+        </div>
+        <div className="w-10 h-10 rounded-md flex justify-center items-center hover:bg-slate-200 duration-300">
+          <Image src={ICON_INSTAGRAM} alt="instagram" width={28} height={28} />
+        </div>
       </div>
-    </div>
+    </header>
   );
 }
 
