ef45088009b1cc8e9bf2d9a3301b75aaccebee38
Sunwoo-An <110546006+Sunja-An@users.noreply.github.com>
2025-02-17 23:22:34 +0900

    ✨ widget layer upated

--- src/widgets/my/ui/MyProjectList.tsx
+++ src/widgets/my/ui/MyProjectList.tsx
@@ -1,7 +1,21 @@
 "use client";
 
+import { T_SingleProject } from "@/entity/project/project";
+import { useEffect, useState } from "react";
+import { getMyProjects } from "@/widgets/my/api/getMyInfo";
+
 function MyProjectList() {
-  return <div>MyProjectList</div>;
+  const [data, setData] = useState<T_SingleProject[] | false>(false);
+  useEffect(() => {
+    getMyProjects()
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+  return <div className="w-full flex justify-start items-start"></div>;
 }
 
 export { MyProjectList };
